f6b84e23eda831f33b29d4f4d363acf3e6273021
sunnyboy-rasebeka <ST10497918@rcconnect.edu.za>
2025-11-18 15:06:53 +0200

    contact us
    Map added

--- js/myscript.js
+++ js/myscript.js
@@ -28,3 +28,13 @@ function validate () {
     let car = document.getElementById("carSelection").value
 
 }
+
+var map = L.map('map').setView([-26.2041, 28.0473], 6);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Velocity Motors Map'
+}).addTo(map)
+
+L.marker([-26.2041, 28.0473])
+    .addTo(map)
+    .bindPopup("<b>Velocity Motors - Pretoria</b><br>Head Office")
