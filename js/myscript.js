9c65389df2aa53581a3df31a831a709992499278
sunnyboy-rasebeka <ST10497918@rcconnect.edu.za>
2025-11-18 14:19:40 +0200

    javascript for services
    Function for services has been added.

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,5 @@
 
+
 function openLightbox(imageSrc) {
     document.getElementById("lightbox-img").src = imageSrc;
     document.getElementById("lightbox").style.display = "block";
@@ -7,3 +8,16 @@ function openLightbox(imageSrc) {
 function closeLightbox() {
     document.getElementById("lightbox").style.display = "none";
 }
+
+
+function validate () {
+    let name = document.getElementById("cname").value
+    let email = document.getElementById("cemail").value
+    let reason = document.getElementById("reason").value
+    let msg = document.getElementById("message").value
+
+    if (name === "" || email === "" || reason === "" || msg === "") {
+        alert("Please fill in all fields before submitting.");
+        return;
+}
+
